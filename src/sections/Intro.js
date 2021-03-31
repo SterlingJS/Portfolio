4c9bd0557a9ea8009b96fb3251add05df8eda4db
Jim <jws6720@rit.edu>
2021-03-31 09:43:28 -0400

    Added simple zoom in for the parts of the intro

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -1,29 +1,38 @@
 import IntroModal from '../components/intro-modal'
 import $ from 'jquery';
 import './section.css';
-import './intro.css';
+import './intro.scss';
 
 
 function Intro () {
 
 	const beginIntro = () => {
-		$('.big-letter').toggleClass('transform-active');
-		$('.regular-letters').toggleClass('transform-active');
+		$('.large-underline').toggleClass('final-title');
+		$('.first-name').toggleClass('final-title');
+		$('.last-name').toggleClass('final-title');
+		$('.small-underlines').toggleClass('final-title');
 		var audio = document.getElementById("strangerAudio");
   		audio.volume = 0.7;
 		audio.play()
 	}
 
 	return (
-		<div className='intro-container'>
+		<div >	
 			<IntroModal beginIntro={beginIntro} />
-			<div className='first-name'>
-				<p className='big-letter'>J</p>
-				<p className='regular-letters'>AME</p>
-				<p className='big-letter'>S</p>
-			</div>
-			<div className='last-name'>
-				<p className='regular-letters'>STERLING</p>
+			<div className='intro-container'>
+				<div className='large-underline' />
+				<div className='first-name'>
+					{/* <p className='big-letter'>J</p>
+					<p className='regular-letters'>AME</p>
+					<p className='big-letter'>S</p> */}
+				</div>
+				<div className='last-name'>
+					{/* <p className='regular-letters'>STERLING</p> */}
+				</div>
+				<div className='small-underlines' >
+					{/* <div className='left-underline' />
+					<div className='right-underline' /> */}
+				</div>
 			</div>
 		</div>
 	)
