761c1ddf6ab3eebce55ec4ab0ff238e2baf3b197
Jim <jws6720@rit.edu>
2021-04-08 08:47:00 -0400

    Increased starting zoom to 10x

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -21,6 +21,14 @@ function Intro () {
 		$('.james-m').toggleClass('final-letter');
 		$('.james-e').toggleClass('final-letter');
 		$('.james-s').toggleClass('final-letter');
+		$('.sterling-s').toggleClass('final-letter');
+		$('.sterling-t').toggleClass('final-letter');
+		$('.sterling-e').toggleClass('final-letter');
+		$('.sterling-r').toggleClass('final-letter');
+		$('.sterling-l').toggleClass('final-letter');
+		$('.sterling-i').toggleClass('final-letter');
+		$('.sterling-n').toggleClass('final-letter');
+		$('.sterling-g').toggleClass('final-letter');
 		var audio = document.getElementById("strangerAudio");
   		audio.volume = 0.7;
 		audio.loop = false;
@@ -33,16 +41,23 @@ function Intro () {
 		<div>	
 			<IntroModal beginIntro={beginIntro} />
 			<div className='intro-container'>
-				<div className='large-underline' />
+				<div className='large-underline'>_________</div>
+				<div className='last-name'>
+					<p className='sterling-s'>S</p>
+					<p className='sterling-t'>T</p>
+					<p className='sterling-e'>E</p>
+					<p className='sterling-r'>R</p>
+					<p className='sterling-l'>L</p>
+					<p className='sterling-i'>I</p>
+					<p className='sterling-n'>N</p>
+					<p className='sterling-g'>G</p>
+				</div>
 				<div className='first-name'>
 					<p className='james-j'>J</p>
 					<p className='james-a'>A</p>
 					<p className='james-m'>M</p>
 					<p className='james-e'>E</p>
 					<p className='james-s'>S</p>
-				</div>
-				<div className='last-name'>
-					{/* <p className='regular-letters'>STERLING</p> */}
 				</div>
 				<div className='small-underlines' >
 					{/* <div className='left-underline' />
